b9d1ac25ade44ae639282dc84b1fb6334b7f9892
Chen Zhi Qi <csczq123456@126.com>
2018-01-12 21:26:59 +0800

    Some bugs fixed.

--- Code/main.c
+++ Code/main.c
@@ -21,13 +21,13 @@ FILE* out;
 int main(int argc, char** argv)
 {
     FILE* f = fopen(argv[1], "r");
-    out = fopen(argv[2], "w");
     if (!f)
     {
         printf("parser:\033[0;31m fatal error:\033[0m no input file\n");
         printf("compilation terminated.\n");
         return 1;
     }
+    out = fopen(argv[2], "w");
     if (!out)
     {
         printf("parser:\033[0;31m fatal error:\033[0m no output file\n");
